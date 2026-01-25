db4ee1eec2c95c2a340dd9428a7f3c3c8830abbe
Saksham Pandey <sakshampandey9695@gmail.com>
2026-01-25 17:34:25 +0530

    send auth token in api headers

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,7 +1,19 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState()?.auth?.userInfo?.token;
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   baseQuery,
